e2d97a7d0ec26673c4617143a5db8f0edff8c99e
MH Parvez <parvez.cse2021@gmail.com>
2024-12-23 15:41:19 +0600

    Update db.mongo.ts

--- src/utils/db.mongo.ts
+++ src/utils/db.mongo.ts
@@ -2,15 +2,11 @@
 import mongoose from "mongoose";
 
 export const connectMongoDB = async (uri: string) => {
-	try {
-		const db: string = "ecommerceTest"
-		await mongoose.connect(`${uri}/${db}`);
-		console.log(`DB: Connected`);
-
-	} catch (error) {
-		console.log(error);
-		console.log("DB: Not Connected");
-		process.exit(1);
-	}
+	mongoose
+	.connect(uri, {
+		dbName: "sylheiDukan",
+	})
+	.then((c) => console.log(`DB Connected to ${c.connection.host}`))
+	.catch((e) => console.log(e));
 };
 
